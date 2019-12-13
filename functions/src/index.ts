275911799d1495f3c65fb1c49ec482472d5aadfc
심철환 <a@akaiv.com>
2019-12-13 18:54:55 +0900

    Functions: 데이터베이스를 가져오기

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,19 @@
 import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+admin.initializeApp()
 
-export const api = functions.https.onRequest((request, response) => {
-  response.send('api')
+export const api = functions.https.onRequest(async (request, response) => {
+  const start = Date.now()
+  const snapshot = await admin
+    .database()
+    .ref('/')
+    .once('value')
+  const end = Date.now()
+
+  const result = {
+    duration: end - start,
+    type: typeof snapshot
+  }
+
+  response.send(result)
 })
