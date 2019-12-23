b41d364b40469957d1a5f40e24e8f26c9a0f9f88
심철환 <a@akaiv.com>
2019-12-23 22:24:09 +0900

    기능: PWA - 서비스워커 등록

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
+import * as serviceWorker from './serviceWorker'
 import './index.scss'
 import App from './components/App'
 
 render(<App />, document.getElementById('movies'))
+serviceWorker.register()
